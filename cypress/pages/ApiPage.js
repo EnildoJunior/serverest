08b00631b87ee684c39d85245224e8e9c776ef8f
Enildo Junior <enildobernardojunior@gmail.com>
2026-05-20 22:25:49 -0300

    adicionando comentários

--- cypress/pages/ApiPage.js
+++ cypress/pages/ApiPage.js
@@ -1,6 +1,9 @@
 const loc = require('../locators/ApiLocator')
 
 class ApiPage {
+  // Realiza login na API e retorna a resposta com o token de autorização
+  // @param {string} email - E-mail do usuário
+  // @param {string} password - Senha do usuário
   login(email, password) {
     return cy.request({
       method: 'POST',
@@ -10,6 +13,11 @@ class ApiPage {
     })
   }
 
+  // Cria um novo usuário na API
+  // @param {string} nome - Nome completo do usuário
+  // @param {string} email - E-mail do usuário
+  // @param {string} password - Senha do usuário
+  // @param {string} administrador - Define se é admin ('true' ou 'false', padrão 'false')
   criarUsuario(nome, email, password, administrador = 'false') {
     return cy.request({
       method: 'POST',
@@ -19,6 +27,12 @@ class ApiPage {
     })
   }
 
+  // Cria um novo produto na API (requer token de administrador)
+  // @param {string} nome - Nome do produto
+  // @param {number} preco - Preço do produto
+  // @param {string} descricao - Descrição do produto
+  // @param {number} quantidade - Quantidade em estoque
+  // @param {string} token - Token de autorização Bearer do administrador
   criarProduto(nome, preco, descricao, quantidade, token) {
     return cy.request({
       method: 'POST',
